c7b84f57d2f75dfd5c7b444ebc8ac7a89344fb06
lucas <mlpabcd@sina.com>
2016-03-14 17:43:53 +0800

    test add Method

--- app/src/main/java/com/lucasfamily/testforgithub/MainActivity.java
+++ app/src/main/java/com/lucasfamily/testforgithub/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -26,6 +27,12 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        testGithub();
+    }
+
+    public void testGithub() {
+        Log.i("MainActivity","..."+ "testHool...");
     }
 
     @Override
